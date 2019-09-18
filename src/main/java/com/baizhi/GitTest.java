00ca6e4ec86f722ebd2777ce3e50f61249b55c58
yang <648590719@qq.com>
2019-09-18 17:18:04 +0800

    var 0.0

--- src/main/java/com/baizhi/GitTest.java
+++ src/main/java/com/baizhi/GitTest.java
@@ -1,7 +1,5 @@
 package com.baizhi;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("111111");
@@ -11,5 +9,7 @@ public class GitTest {
         System.out.println("我在github");
         System.out.println("哈哈哈");
         System.out.println("我在Idea!!!");
+        System.out.println("fdjsf");
+        System.out.println("hjfshjf");
     }
 }

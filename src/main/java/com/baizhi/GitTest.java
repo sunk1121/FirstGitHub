15aee820ba16b497670818badeae611477580f76
sunwk <626378360@qq.com>
2019-09-18 16:41:22 +0800

    2.0

--- src/main/java/com/baizhi/GitTest.java
+++ src/main/java/com/baizhi/GitTest.java
@@ -5,6 +5,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("111111");
-        System.out.println("222222");
+        System.out.println("222222--1.0");
+        System.out.println("333333");
+        System.out.println("444444--2.0");
     }
 }

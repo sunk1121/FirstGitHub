917f82d4c3a20c644a41cecb6a831c24edd70f8b
sunk1121 <55435884+sunk1121@users.noreply.github.com>
2019-09-18 16:55:56 +0800

    Update GitTest.java

--- src/main/java/com/baizhi/GitTest.java
+++ src/main/java/com/baizhi/GitTest.java
@@ -8,6 +8,7 @@ public class GitTest {
         System.out.println("222222--1.0");
         System.out.println("333333");
         System.out.println("444444--2.0");
-        Sustem.out.println("我在github");
+        System.out.println("我在github");
+        System.out.println("哈哈哈");
     }
 }

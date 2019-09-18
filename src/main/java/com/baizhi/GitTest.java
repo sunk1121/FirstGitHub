d9f1123f80710de59847981726c3e3dd28aad993
sunwk <626378360@qq.com>
2019-09-18 16:35:29 +0800

    1.0

---
+++ src/main/java/com/baizhi/GitTest.java
@@ -0,0 +1,10 @@
+package com.baizhi;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("111111");
+        System.out.println("222222");
+    }
+}
